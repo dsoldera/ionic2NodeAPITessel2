623697d7485af8b212b198a44e93b19a5aee92bb
dsoldera <daniellesoldera@gmail.com>
2017-03-23 20:49:26 -0300

    update files

--- api/server.js
+++ api/server.js
@@ -4,7 +4,31 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+var five = require('johnny-five');
+var tessel = require('tessel-io');
+
+var board = new five.Board({
+  io: new tessel()
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+
+app.get('/api/contacts', (request, response) => {
+
+});
+
+app.get('/api/contacts/:id', (request, response) => {
+
+});
+
+const hostname = 'localhost';
+const port = 3001;
+
+const server = app.listen(port, hostname, () => {
+
+  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`To stop running CTRL + C`);
+});
--- tessel2/blink.js
+++ tessel2/blink.js
@@ -5,9 +5,9 @@ var board = new five.Board({
   io: new Tessel()
 });
 
-board.on("ready", function() {
-  var led = new five.Led("a0");
-  led.blink(500);
-});
+// board.on("ready", function() {
+//   var led = new five.Led("a0");
+//   led.blink(500);
+// });
 
 console.log("Pisca! (Aperte CTRL + C para interromper)");
